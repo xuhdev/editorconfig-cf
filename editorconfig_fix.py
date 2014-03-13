3b78217f1fdeba63cb63dff1113adb0e149b1b75
Hong Xu <hong@topbug.net>
2014-03-13 01:24:27 -0700

    Initial commit.

---
+++ editorconfig_fix.py
@@ -0,0 +1,157 @@
+#!/usr/bin/python
+
+from __future__ import print_function
+
+BEAUTIFIER_OPTIONS_CONFIG = 1
+BEAUTIFIER_OPTIONS_INPUT = 2
+BEAUTIFIER_OPTIONS_OUTPUT = 3
+
+global_options = {
+        'editorconfig':      'editorconfig',
+        'output_suffix':     '.ecfix'
+        }
+
+extension_language_map = {
+        'c': 'c',
+        'cpp': 'cpp',
+        'h': 'cpp',
+        'hpp': 'cpp'
+        }
+
+class EditorConfigFixError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+def uncrustify_handling(executable, input_file_path, output_file_path, editorconfig_properties):
+    import subprocess
+    import sys
+    import tempfile
+
+    # The temp config file
+    cfg = tempfile.NamedTemporaryFile(mode='w', suffix='.cfg')
+    for key, value in editorconfig_properties.items():
+        if key == 'tab_width':
+            cfg.write('input_tab_size = ' + value + '\n')
+            cfg.write('output_tab_size = ' + value + '\n')
+        if key == 'indent_size':
+            if value == 'tab':
+                if 'tab_width' in editorconfig_properties:
+                    cfg.write('indent_columns = ' + cfg['tab_width'] + '\n')
+                else:
+                    cfg.write('indent_columns = 8\n')
+            else:
+                cfg.write('indent_columns = ' + value + '\n')
+        if key == 'indent_style':
+            if value == 'space':
+                cfg.write('indent_with_tabs = 0\n')
+            elif value == 'tab':
+                cfg.write('indent_with_tabs = 1\n')
+        if key == 'end_of_line':
+            cfg.write('newlines = ' + value + '\n')
+
+    # Need to write to disk
+    cfg.flush()
+
+    # execute uncrustify
+    cmd = [executable, '-c', cfg.name, '-f', input_file_path, '-o', output_file_path]
+    print('Executing: ' + ' '.join(cmd), file=sys.stderr)
+    return subprocess.call(cmd, shell=False)
+
+
+def get_language_info(lang):
+    """
+    Obtain the information needed for a given language.
+    """
+
+    if lang == 'c':
+        return dict(
+                id=             'c',
+                name=           'C',
+                beautifier=     ['uncrustify'])
+    elif lang == 'cpp':
+        return dict(
+                id=             'cpp',
+                name=           'C++',
+                beautifier=     ['uncrustify'])
+    else:
+        return None
+
+def get_beautifier_info(lang, beautifier):
+    """
+    Obtain the information needed for a given beautifier.
+    """
+
+    if beautifier == 'uncrustify':
+        return dict(
+                id=             'uncrustify',
+                name=           'Uncrustify',
+                language=       'c',
+                executable=     'uncrustify',
+                func_handling=  uncrustify_handling)
+    else:
+        return None
+
+def fix_one_file(file_path):
+    """
+    Fix one file with a file path file_path
+    """
+    import os
+    import editorconfig
+
+    _, ext = os.path.splitext(file_path)
+
+    # check the corresponding language
+    ext = ext[1:]
+    if ext not in extension_language_map:
+        return 2
+
+    lang = extension_language_map[ext]
+    lang_info = get_language_info(lang)
+
+    # get editorconfig properties
+    ec_properties = editorconfig.get_properties(file_path)
+
+    succeed = -1
+    for b in lang_info['beautifier']:
+        b_info = get_beautifier_info(lang, b)
+        # If the spawning succeeds, then we are finished; otherwise, let's try
+        # the next beautifier 
+        if b_info['func_handling'](b_info['executable'], file_path, file_path + global_options['output_suffix'], ec_properties) == 0:
+            succeed = 0
+            break
+
+    if succeed != 0:
+        raise EditorConfigFixError('Failed to fix "' + file_path + '"')
+
+def fix_files(file_path):
+    """
+    Recursively fix the file with a path of file_path 
+    """
+    import os,sys
+
+    if os.path.isdir(file_path):
+        for f in os.listdir(file_path):
+            fix_files(os.path.join(file_path, f))
+
+    try:
+        fix_one_file(file_path)
+    except EditorConfigFixError as e:
+        print('Warning: ' + e, file=sys.stderr)
+
+def main():
+    import sys
+    import os
+    import getopt
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], '')
+    except:
+        print('Argument error.', file=sys.stderr)
+        sys.exit(1)
+
+    fix_files(os.path.abspath(args[0]))
+
+if __name__ == '__main__':
+    main()
